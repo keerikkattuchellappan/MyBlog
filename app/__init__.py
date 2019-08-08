3a8b3e72515836d99f80d2c6ae1ed2ed4bb33857
JoePySparker <pysparker@gmail.com>
2019-08-07 21:20:55 -0400

    forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config 
 
 blog = Flask(__name__)
+blog.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import blog
 from flask import render_template
+from app.forms import LoginForm
+
 
 @blog.route('/')
 @blog.route('/index')
@@ -16,3 +18,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@blog.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title ='Sign In', form = form)
+
