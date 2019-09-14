bd9c88030c1c444f2e583eee4c62ee9a18ecc516
JoePySparker <pysparker@gmail.com>
2019-09-14 10:26:06 -0400

    Db check

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 blog = Flask(__name__)
 blog.config.from_object(Config)
+db = SQLAlchemy(blog)
+migrate = Migrate(blog,db)
 
-from app import routes
+from app import routes, models #models needs to be added.
+
+
+
