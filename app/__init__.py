6417369581c76051adcc47c4ee2880e36e56cbb7
JoePySparker <pysparker@gmail.com>
2019-09-14 15:39:59 -0400

    db added

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config import Config 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 blog = Flask(__name__)
 blog.config.from_object(Config)
 db = SQLAlchemy(blog)
 migrate = Migrate(blog,db)
+login = LoginManager(blog)
+login.login_view = 'login'
 
 from app import routes, models #models needs to be added.
 
